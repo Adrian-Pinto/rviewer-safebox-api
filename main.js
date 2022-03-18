e160280091c12e7c37b632e3af6d2f7a37a39583
Adrian Pinto <adripintogonzalez@gmail.com>
2022-03-18 12:24:13 +0100

    add database

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 import 'dotenv/config';
 import fs from 'fs';
 import initAPI from './src/api.js';
-// import { createConection, getCon } from './src/config/lowdbConfig';
+import { createConnection, getConnection } from './src/config/lowdbConfig.js';
 
 const PORT = process.env.PORT || 3001;
 
 const key = fs.readFileSync('./src/cert/api.key', 'utf8');
 const crt = fs.readFileSync('./src/cert/api.crt', 'utf8');
 
-// createConection();
+createConnection();
 
 initAPI({
   port: PORT,
-  services: null,
+  services: getConnection,
   cert: {
     key,
     crt,
---
+++ src/config/lowdbConfig.js
@@ -0,0 +1,21 @@
+import { LowSync, JSONFileSync } from 'lowdb';
+
+let database;
+
+const createConnection = () => {
+  database = new LowSync(new JSONFileSync('./database/db.json'));
+
+  database.read();
+  database.data ||= {
+    boxes: [],
+    boxContent: [],
+  };
+  database.write();
+};
+
+const getConnection = () => database;
+
+export {
+  createConnection,
+  getConnection,
+};
