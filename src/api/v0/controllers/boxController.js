1c6d32780915beae0e03145df0521164f24987b2
Adrian Pinto <adripintogonzalez@gmail.com>
2022-03-20 11:12:11 +0100

    add empty func of controllers add notes of desired imports

---
+++ src/api/v0/controllers/boxController.js
@@ -0,0 +1,20 @@
+// import getNewId
+// import boxContentSchema
+
+const postNewBox = (req, res) => {
+
+};
+
+const getBoxById = (req, res) => {
+
+};
+
+const putBoxById = (req, res) => {
+
+};
+
+export default {
+  postNewBox,
+  getBoxById,
+  putBoxById,
+};
