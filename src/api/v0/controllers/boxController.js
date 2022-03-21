44e700c8eb16e7a2343adbe72881c42ed662ada3
Adrian Pinto <adripintogonzalez@gmail.com>
2022-03-21 20:12:53 +0100

    add postNewBox function

--- src/api/v0/controllers/boxController.js
+++ src/api/v0/controllers/boxController.js
@@ -1,20 +1,49 @@
-// import getNewId
+import getNewId from '../../../utils/getNewId.js';
+import { encryptString, decryptString } from '../../../utils/ciphString.js';
+import doHash from '../../../utils/doHash.js';
 // import boxContentSchema
 
-const postNewBox = (req, res) => {
+const postNewBox = ({ services, body: { name, password } }, res, next) => {
+  const isPasswordStrong = /(?=(.*[A-Z]){2,})(?=(.*[!@#$&*]){1,})(?=(.*[0-9]){2,})(?=(.*[a-z]){3,}).{12,}/.test(password);
+  const isName = !!name.trim().length;
+  const isPassword = !!password.trim().length;
+
+  if (!(isPasswordStrong && isName && isPassword)) return next({ status: 422, message: 'Malformed expected data' });
+
+  const isBox = services.getDatabase().data.boxes.find((box) => box.boxName === name);
+
+  if (isBox) return next({ status: 409, message: 'Safebox already exist' });
+
+  const boxId = getNewId();
+  const boxContentId = getNewId();
+
+  services.getDatabase().data.boxes.push({
+    id: boxId,
+    boxName: name,
+    boxPassword: doHash(password),
+    boxContentId,
+  });
+
+  services.getDatabase().data.boxContent.push({
+    id: boxContentId,
+    items: [],
+  });
+
+  services.getDatabase().write();
+
+  res.status(200).send({ id: boxId });
+};
+
+const getBoxItemsById = (req, res) => {
 
 };
 
-const getBoxById = (req, res) => {
-
-};
-
-const putBoxById = (req, res) => {
+const putNewBoxItemById = (req, res) => {
 
 };
 
 export default {
   postNewBox,
-  getBoxById,
-  putBoxById,
+  getBoxItemsById,
+  putNewBoxItemById,
 };
