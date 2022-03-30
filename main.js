dcdd28e7d56c1c954d806053dba5635a8be5b022
Adrian Pinto <adripintogonzalez@gmail.com>
2022-03-30 17:53:48 +0200

    take out https server listen from initAPI to main, these way we can solve api init issues on test suites

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
+import { stdout } from 'process';
 import fs from 'fs';
+import https from 'https';
 import initAPI from './src/initApi.js';
 import { createConnection, getDatabase } from './src/config/lowdbConfig.js';
 
@@ -7,16 +9,15 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
-const key = fs.readFileSync('./src/cert/api.key', 'utf8');
-const crt = fs.readFileSync('./src/cert/api.crt', 'utf8');
-
 createConnection();
 
-initAPI({
-  port: PORT,
+https.createServer({
+  key: fs.readFileSync('./src/cert/api.key', 'utf8'),
+  cert: fs.readFileSync('./src/cert/api.crt', 'utf8'),
+  rejectUnauthorized: false,
+}, initAPI({
   services: { getDatabase },
-  cert: {
-    key,
-    crt,
-  },
-});
+})).listen(
+  PORT,
+  () => stdout.write(`Server runing on port ${PORT}\n`),
+);
--- src/initApi.js
+++ src/initApi.js
@@ -1,12 +1,10 @@
-import { stdout } from 'process';
-import https from 'https';
 import express from 'express';
 import servicesInjector from './api/v0/middlewares/servicesInjector.js';
 import errorHandler from './utils/errorHandler.js';
 import notFoundHandler from './utils/notFoundHandler.js';
 import boxRouter from './api/v0/routes/boxRouter.js';
 
-const initAPI = ({ cert, port, services }) => {
+const initAPI = ({ services }) => {
   const api = express();
 
   api.disable('x-powered-by');
@@ -20,17 +18,6 @@ const initAPI = ({ cert, port, services }) => {
   api.use(notFoundHandler);
 
   api.use(errorHandler);
-
-  https.createServer({
-    key: cert.key,
-    cert: cert.crt,
-    ca: cert.ca,
-    rejectUnauthorized: false,
-  }, api)
-    .listen(
-      port,
-      () => stdout.write(`Server runing on port ${port}\n`),
-    );
 
   return api;
 };
