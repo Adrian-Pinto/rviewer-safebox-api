6c6e5b11087b5bea463e1820930f5d1d53917677
Adrian Pinto <adripintogonzalez@gmail.com>
2022-03-31 23:36:05 +0200

    fix v1 text case align

--- src/test/v1.api.test.js
+++ src/test/v1.api.test.js
@@ -24,9 +24,9 @@ describe('testing v1 endpoints', () => {
 
   describe('Given: POST call on /v1/safebox', () => {
     describe(`When: Request body includes name: String
-    * : Request body includes password: String`, () => {
+        * : Request body includes password: String`, () => {
       it(`Then: Response status 200
-      * : JSON with key id: String`, (done) => {
+            * : JSON with key id: String`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send({
@@ -34,14 +34,14 @@ describe('testing v1 endpoints', () => {
             password: 'AA@01abcQW12',
           }).end((err, res) => {
             expect(res.status).to.be.equal(200);
-            expect(res.body).to.have.own.property('id').that.is.a('id');
+            expect(res.body).to.have.own.property('id').that.is.a('String');
             done();
           });
       });
     });
     describe('When: Request body includes existing safebox name', () => {
       it(`Then: Response status 409
-      * : Response text 'Safebox already exists'`, (done) => {
+            * : Response text 'Safebox already exist'`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send({
@@ -55,7 +55,7 @@ describe('testing v1 endpoints', () => {
                 password: 'AA@01abcQW12',
               }).end((err, res) => {
                 expect(res.status).to.be.equal(409);
-                expect(res.text).to.be.equal('Safebox already exists');
+                expect(res.text).to.be.equal('Safebox already exist');
                 done();
               });
           });
@@ -63,7 +63,7 @@ describe('testing v1 endpoints', () => {
     });
     describe('When: Request body missing name or password', () => {
       it(`Then: Response status 422
-          * : Response text 'Malformed expected data'`, (done) => {
+            * : Response text 'Malformed expected data'`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send()
@@ -76,20 +76,20 @@ describe('testing v1 endpoints', () => {
     });
     describe('When: Request missing endpoint', () => {
       it(`Then: Response status 404
-      * : Response text 'Requested endpoint does not exist'`, (done) => {
+            * : Response text 'Requested endpoint does not exist'`, (done) => {
         chai.request(api)
           .post('/v1/unexistEndpoint')
           .send()
           .end((err, res) => {
             expect(res.status).to.be.equal(404);
-            expect(res.text).to.be.equal('Requested enpoint does not exist');
+            expect(res.text).to.be.equal('Requested endpoint does not exist');
             done();
           });
       });
     });
     describe('When: Occurs an unknown API error', () => {
       it(`Then: Response status 500
-      * : Response text 'Unexpected API error'`, (done) => {
+            * : Response text 'Unexpected API error'`, (done) => {
         const mockResponse = () => ({
           status: sinon.stub().returnsThis(),
           send: sinon.stub().returnsThis(),
@@ -107,9 +107,9 @@ describe('testing v1 endpoints', () => {
   });
   describe('Given: GET call on /v1/safebox/{id}/open', () => {
     describe(`When: Safebox id exist
-    * : Auth header is correct`, () => {
+        * : Auth header is correct`, () => {
       it(`Then: Response status 200
-      * : JSON with key token: String`, (done) => {
+            * : JSON with key token: String`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send({
@@ -129,7 +129,7 @@ describe('testing v1 endpoints', () => {
     });
     describe('When: Safebox id not exist', () => {
       it(`Then: Response status 404
-      * : Response text 'Requested safebox does not exist'`, (done) => {
+            * : Response text 'Requested safebox does not exist'`, (done) => {
         chai.request(api)
           .get('/v1/safebox/unexistId/open')
           .auth('testv1_box', 'AA@01abcQW12')
@@ -141,9 +141,9 @@ describe('testing v1 endpoints', () => {
       });
     });
     describe(`When: Safebox id exist
-    But: Auth header is missing`, () => {
+        But: Auth header is missing`, () => {
       it(`Then: Response status 422
-      * : Response text 'Malformed expected data'`, (done) => {
+            * : Response text 'Malformed expected data'`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send({
@@ -161,9 +161,9 @@ describe('testing v1 endpoints', () => {
       });
     });
     describe(`When: Safebox id exist
-    But: Safebox is locked`, () => {
+        But: Safebox is locked`, () => {
       it(`Then: Response status 423
-    * : Response text 'Requested safebox is locked'`, (done) => {
+            * : Response text 'Requested safebox is locked'`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send({
@@ -185,16 +185,16 @@ describe('testing v1 endpoints', () => {
     });
     describe('When: Occurs an unknown API error', () => {
       it.skip(`Then: Response status 500
-      * : Response text 'Unexpectted API error'`, (_done) => {
+            * : Response text 'Unexpectted API error'`, (_done) => {
 
       });
     });
   });
   describe('Given: GET call on /v1/safebox/{id}/items', () => {
     describe(`When: Safebox id exist
-    * : Auth token is correct`, () => {
+        * : Auth token is correct`, () => {
       it(`Then: Response status 200
-    * : Response JSON with items Array`, (done) => {
+            * : Response JSON with items Array`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send({
@@ -221,7 +221,7 @@ describe('testing v1 endpoints', () => {
     describe(`When: Safebox id exist
         But: Auth token is not valid`, () => {
       it(`Then: Response status 401
-        * : Response text 'Specified token does not match'`, (done) => {
+            * : Response text 'Specified token does not match'`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send({
@@ -242,7 +242,7 @@ describe('testing v1 endpoints', () => {
     });
     describe('When: Safebox id not exist', () => {
       it(`Then: Response status 404
-        * : Response text 'Requested safebox does not exist'`, (done) => {
+            * : Response text 'Requested safebox does not exist'`, (done) => {
         chai.request(api)
           .get('/v1/safebox/unexistId/items')
           .auth('ValidToken', { type: 'bearer' })
@@ -254,9 +254,9 @@ describe('testing v1 endpoints', () => {
       });
     });
     describe(`When: Safebox exist
-      But: Auth header is missing`, () => {
+        But: Auth header is missing`, () => {
       it(`Then: Response status 422
-        * : Response text 'Malformed expected data'`, (done) => {
+            * : Response text 'Malformed expected data'`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send({
@@ -275,9 +275,9 @@ describe('testing v1 endpoints', () => {
       });
     });
     describe(`When: Safebox id exist
-      But: Safebox is locked`, () => {
+       But: Safebox is locked`, () => {
       it(`Then: Response status 423
-        * : Response text 'Requested safebox is locked'`, (done) => {
+            * : Response text 'Requested safebox is locked'`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send({
@@ -305,17 +305,17 @@ describe('testing v1 endpoints', () => {
     });
     describe('When: Occurs an unknown API error', () => {
       it.skip(`Then: Response status 500
-        * : Response text 'Unexpected API error'`, (_done) => {
+            * : Response text 'Unexpected API error'`, (_done) => {
 
       });
     });
   });
   describe('Given: PUT call on /v1/safebox/{id}/items', () => {
     describe(`When: Safebox id exist
-      * : Auth header token is valid
-      * : Request body is a valid items Array`, () => {
+       * : Auth header token is valid
+       * : Request body is a valid items Array`, () => {
       it(`Then: Response status 200
-      * : Response text 'Content correctly added to the safebox'`, (done) => {
+            * : Response text 'Content correctly added to the safebox'`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send({
@@ -347,9 +347,9 @@ describe('testing v1 endpoints', () => {
       });
     });
     describe(`When: Safebox id exist
-      But: Auth header is not valid`, () => {
+       But: Auth header is not valid`, () => {
       it(`Then: Response status 401
-        * : Response text 'Specified token does not match'`, (done) => {
+            * : Response text 'Specified token does not match'`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send({
@@ -381,7 +381,7 @@ describe('testing v1 endpoints', () => {
     });
     describe('When: Safebox id not exist', () => {
       it(`Then: Response status 404
-        * : Response text 'Requested safebox does not exist'`, (done) => {
+            * : Response text 'Requested safebox does not exist'`, (done) => {
         chai.request(api)
           .put('/v1/safebox/unexistId/items')
           .end((err, res) => {
@@ -392,9 +392,9 @@ describe('testing v1 endpoints', () => {
       });
     });
     describe(`When: Safebox id exist
-      But: Request body is not a valid items Array`, () => {
+        But: Request body is not a valid items Array`, () => {
       it(`Then: Response status 422
-        * : Response text 'Malformed expected data'`, (done) => {
+            * : Response text 'Malformed expected data'`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send({
@@ -423,9 +423,9 @@ describe('testing v1 endpoints', () => {
       });
     });
     describe(`When: Safebox id exist
-      But: Safebox is locked`, () => {
+       But: Safebox is locked`, () => {
       it(`Then: Response status 423
-        * : Response text 'Requested safebox is locked'`, (done) => {
+            * : Response text 'Requested safebox is locked'`, (done) => {
         chai.request(api)
           .post('/v1/safebox')
           .send({
@@ -459,7 +459,7 @@ describe('testing v1 endpoints', () => {
     });
     describe('When: Occurs an unknown API error', () => {
       it.skip(`Then: Response status 500
-        * : Response text 'Unexpected API error'`, (_done) => {
+            * : Response text 'Unexpected API error'`, (_done) => {
 
       });
     });
