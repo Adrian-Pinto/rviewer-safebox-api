ba477b6c302ab1a8bf6c90bfed414ffc88d79984
Adrian Pinto <adripintogonzalez@gmail.com>
2022-04-01 00:06:26 +0200

    add basicAuth on authorizeUser

--- src/api/v1/middlewares/authorizeUser.js
+++ src/api/v1/middlewares/authorizeUser.js
@@ -1,24 +1,24 @@
-export const basicAuth = ({ params: { boxId }, headers: { authorization } }, res, next) => {
-  /*
+import doHash from '../../../utils/doHash.js';
+import boxSchema from '../../v0/models/boxModel.js';
+
+export const basicAuth = (req, res, next) => {
+  const { params: { id }, headers: { authorization } } = req;
   const trimPassword = Buffer.from(
     authorization?.split(' ')[1].toString() || '',
     'base64',
-  ).toString('ascii').split(:)[1].trim();
+  ).toString('ascii').split(':')[1]?.trim();
 
-  if (!authorization && !boxSchema({ boxId, trimPassword })) retrun next({ status: 422, message: 'Malformed expected data' })
+  if (!authorization && !boxSchema({ id, trimPassword })) return next({ status: 422, message: 'Malformed expected data' });
 
-  const isBox = req.services.getDatabase().data.boxes.find((box) => box.id === boxId);
+  const isBox = req.services.getDatabase().data.boxes.find((box) => box.id === id);
 
   if (!isBox) return next({ status: 404, message: 'Requested safebox does not exist' });
 
   const hashPassword = doHash(trimPassword);
 
-  if (hashPassword !== isBox.password) return next({ status:422, message: 'Malformed expected data' });
+  if (hashPassword !== isBox.password) return next({ status: 422, message: 'Malformed expected data' });
 
   req.boxObject = isBox;
-  next();
-  */
-  console.log('basic auth');
   next();
 };
 
